876b451253d06d09ec472f337203b45886ad150f
Lucasalves09 <157380638+Lucasalves09@users.noreply.github.com>
2024-02-16 01:15:56 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    println("What is your name?")
+    val name = readln()
+    println("Hello, $name")
+}
